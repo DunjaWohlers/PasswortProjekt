a9ef731b586212e040913fb9e85c4e5bc3677aff
dunjawohlers <dunjawohlers@gmx.de>
2022-07-01 16:57:16 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ausgabe();
+    }
+
+    public static void ausgabe(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte gib ein Passwort ein:");
+        String inputStr= sc.nextLine();
+
+        if(hasLowerCase(inputStr)){
+            System.out.println("Dein Passwort enthält Kleinbuchstaben, versuchs noch einmal!");
+            ausgabe();
+        }else{
+            System.out.println("Das Passwort enthält keine KLeinbuchstaben und wir weiter überprüft!");
+        }
+        if(hasLength3To10(inputStr)){
+            System.out.println("Die Länge passt!");
+        }else {
+            System.out.println("Die Länge passt nicht!");
+        }
+        if(hasNumber(inputStr)){
+            System.out.println("Das Passwort enthält mehr als eine Nummer");
+        }else{
+            System.out.println("Das Passwort enthält keine Nummer");
+        }
+
+        boolean perfect=isPerfect(inputStr);
+        System.out.println("Das Passwort ist genial!?" + perfect);
+    }
+    public static boolean hasLength3To10(String str){
+        return str.length()<=10&&str.length()>=3;
+    }
+
+    public static boolean hasNumber(String str){
+        boolean hasNum=false;
+        for(int i=0;i<str.length();i++){
+            char x=pickLetterAt(str,i);
+            if(Character.isDigit(x)){
+                hasNum=true;
+            };
+        }
+        return hasNum;
+    }
+
+    public static char pickLetterAt(String str, int zaehler){
+        return str.charAt(zaehler);
+    }
+
+    public static boolean isPerfect(String str){
+        String regEx="\\D{3,5}\\d{1,3}";
+        return str.matches(regEx);
+    }
+
+    public static boolean hasLowerCase(String str){
+        boolean hasLowerCase=false;
+        for(int i=0;i<str.length();i++){
+            char x=pickLetterAt(str,i);
+            if(Character.isLowerCase(x)){
+                hasLowerCase=true;
+            };
+        }
+        return hasLowerCase;
+    }
+
+    public static boolean isBad(String str) {
+        return str.equalsIgnoreCase("PASSWORT")||str.contains("123")||str.contains("234");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,98 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void hasWrongLength(){
+        //given
+        String str="A8";
+        //when
+        boolean bo=Main.hasLength3To10(str);
+        //then
+        boolean expected=false;
+        Assertions.assertEquals(expected,bo);
+    }
+
+    @Test
+    void isTooLong(){
+        //given
+        String str="A8ASDFASFJASDIOFJASIDFJ$%";
+        //when
+        boolean bo=Main.hasLength3To10(str);
+        //then
+        boolean expected=false;
+        Assertions.assertEquals(expected,bo);
+    }
+
+    @Test
+    void hasCorrectLength10(){
+        //given
+        String str="A8ASDFASFZ";
+        //when
+        boolean bo=Main.hasLength3To10(str);
+        //then
+        boolean expected=true;
+        Assertions.assertEquals(expected,bo);
+    }
+
+    @Test
+    void hasCorrectLength3(){
+        //given
+        String str="A8A";
+        //when
+        boolean bo=Main.hasLength3To10(str);
+        //then
+        boolean expected=true;
+        Assertions.assertEquals(expected,bo);
+    }
+
+    @Test
+    void hasNumber(){
+        //given
+        String str="A8A";
+        //when
+        boolean bo=Main.hasNumber(str);
+        //then
+        boolean expected=true;
+        Assertions.assertEquals(expected,bo);
+    }
+
+    @Test
+    void hasNoNumber(){
+        //given
+        String str="AZSFGH+#(A";
+        //when
+        boolean bo=Main.hasNumber(str);
+        //then
+        boolean expected=false;
+        Assertions.assertEquals(expected,bo);
+    }
+
+    @Test
+    void istSchlecht(){
+        String str="Passwort";
+        boolean bo=Main.isBad(str);
+        boolean exp=true;
+        Assertions.assertEquals(exp,bo);
+    }
+
+    @Test
+    void istNichtSchlecht(){
+        String str="Passwort123";
+        boolean bo=Main.isBad(str);
+        boolean exp=false;
+        Assertions.assertEquals(exp,bo);
+    }
+
+    @Test
+    void istSchlechtZahl(){
+        String str="12345";
+        boolean bo=Main.isBad(str);
+        boolean exp=true;
+        Assertions.assertEquals(exp,bo);
+    }
+
+}
